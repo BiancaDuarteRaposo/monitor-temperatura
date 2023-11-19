9f40da79a9249a335abc79ce5aaffd00603ead49
Bianca Duarte Raposo <bianca.du.raposo@gmail.com>
2023-11-19 15:59:19 -0300

    Inicializando o projeto

---
+++ monitor_temperatura.ino
@@ -0,0 +1,36 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "config.h"
+
+//Instanciando o feed do Adafruit
+AdafruitIO_Feed *termometro_digital = io.feed("termometro-digital");
+//Instanciando a conexão com o sensor de temperatura
+OneWire barramento(D4);
+//Instanciando o sensor de temperatura
+DallasTemperature sensor(&barramento);
+
+void setup() {
+  //Inicialização e conexão com o adafruit
+  Serial.begin(115200);
+  sensor.begin(); 
+  io.connect();
+  while(io.status() < AIO_CONNECTED) {
+    delay(500);
+  }
+}
+
+
+void loop() {
+  //Iniciando o io do adafruit
+  io.run();
+  //Atualizando a temperatura do sensor
+  sensor.requestTemperatures();
+  //Capturando a temperatura do sensor
+  float temperaturaCelsius = sensor.getTempCByIndex(0);
+  //Escrevendo a temperatura no monitor serial
+  Serial.println(temperaturaCelsius);
+  //Enviando a temperatura para o feed do adafruit
+  termometro_digital->save(temperaturaCelsius);
+  //Atualiza a cada 5 segundos devido ao trottle limit da conta gratuita do adafruit
+  delay(5000);
+}
